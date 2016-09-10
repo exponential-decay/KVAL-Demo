e4f9b650c9408996b87b436136b0bf91b65933a0
ross-spencer <all.along.the.watchtower2001@gmail.com>
2016-09-11 01:47:45 +1200

    [feature] More testing.

--- kval-demo.go
+++ kval-demo.go
@@ -5,35 +5,40 @@ import (
    "github.com/exponential-decay/KVAL-Parse"
 ) 
 
-func main() {
+var insert_value = "INS Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key :: Value"
+var get_bucket_contents = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket"   
+var get_value = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key"
+var get_value_from_key_pattern = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> {PAT}"
+var get_key_from_value = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> _ :: Value"
+var get_key_from_value_pattern = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> _ :: {PAT}"
+var does_key_exist = "LIS Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key"
+var does_bucket_exist = "LIS Prime Bucket >> Secondary Bucket >> Tertiary Bucket"
+var delete_bucket = "DEL Prime Bucket >> Secondary Bucket >> Tertiary Bucket"
+var delete_key = "DEL Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key"
+var delete_value = "DEL Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key :: _ "
+var rename_key = "REN Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key => New Key"
+var rename_bucket = "REN Prime Bucket >> Secondary Bucket >> Tertiary Bucket => Third Bucket"
 
-   //var insert_value = "INS Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key :: Value"
-   //var get_value = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket"   
-   //var get_value = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key"
-   //var get_value_from_key_pattern = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> {PAT}"
-   //var get_key_from_value = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> _ :: Value"
-   //var get_key_from_value_pattern = "GET Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> _ :: {PAT}"
-   //var does_key_exist = "LIS Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key"
-   //var does_bucket_exist = "LIS Prime Bucket >> Secondary Bucket >> Tertiary Bucket"
-   //var delete_bucket = "DEL Prime Bucket >> Secondary Bucket >> Tertiary Bucket"
-   //var delete_key = "DEL Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key"
-   var delete_value = "DEL Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key :: _ "
-   //var rename_key = "REN Prime Bucket >> Secondary Bucket >> Tertiary Bucket >>>> Key => Key"
-   //var rename_bucket = "REN Prime Bucket >> Secondary Bucket >> Tertiary Bucket => Third Bucket"
+
+func main() {
    
    fmt.Println("KVAL demo implementation.")
 
-   //kvalparse.Query(insert_value)
-   //kvalparse.Query(get_value)
-   //kvalparse.Query(get_value_from_key_pattern)
-   //kvalparse.Query(get_key_from_value)
-   //kvalparse.Query(get_key_from_value_pattern)
-   //kvalparse.Query(does_key_exist)
-   //kvalparse.Query(does_bucket_exist) 
-   //kvalparse.Query(delete_bucket)
-   //kvalparse.Query(delete_key)
-   kvalparse.Query(delete_value)
-   //kvalparse.Query(rename_key)
-   //kvalparse.Query(rename_bucket)
+   //kvalparse.Parse(insert_value)
+   //kvalparse.Parse(get_value)
+   kq, err := kvalparse.Parse(get_value_from_key_pattern)
+   //kq, err := kvalparse.Parse(get_key_from_value)
+   //kvalparse.Parse(get_key_from_value_pattern)
+   //kvalparse.Parse(does_key_exist)
+   //kvalparse.Parse(does_bucket_exist) 
+   //kvalparse.Parse(delete_bucket)
+   //kvalparse.Parse(delete_key)
+   //kq, err := kvalparse.Parse(delete_value)
+   //kq, err := kvalparse.Parse(rename_key)
+   //kq, err := kvalparse.Parse(rename_bucket)
 
+   if err != nil {
+      fmt.Println(err)
+   }
+   fmt.Println(kq)
 }
